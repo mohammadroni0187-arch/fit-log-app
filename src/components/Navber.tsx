1521be5443fc784f166d2f0ecdfaf116acb84ea1
mohammadroni0187-arch <mohammadroni0187@gamil.com>
2026-09-26 23:44:14 +0600

    Layout change metadata

--- src/components/Navber.tsx
+++ src/components/Navber.tsx
@@ -29,7 +29,7 @@ const Navber = () => {
           <div
             tabIndex={0}
             role="button"
-            className="btn btn-ghost lg:hidden"
+            className="btn btn-ghost  lg:hidden"
           >
             <svg
               aria-label="Menu"
